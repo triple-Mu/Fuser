5164c416d69f7bfc02e2718c5efd7234b5dd4c2a
Naoya Maruyama <nmaruyama@nvidia.com>
2023-02-16 19:43:22 -0800

    update

--- third_party/nvfuser/test/test_gpu3.cpp
+++ third_party/nvfuser/test/test_gpu3.cpp
@@ -1755,15 +1755,15 @@ __global__ void CUDAGeneratedKernel(Tensor<float, 2> T0, Tensor<float, 2> T2) {
   int64_t i7;
   i7 = T0.size[0] * T0.size[1];
   bool b73;
-  b73 = ((nvfuser_index_t)threadIdx.x) < (i7 - i36);
+  b80 = ((nvfuser_index_t)threadIdx.x) < (i7 - i36);
   float f8;
   f8 = (float)(i7);
   float T1[1];
-  if (b73) {
+  if (b80) {
     T1[0]
        = sinf(T0[i37]);
   }
-  if (b73) {
+  if (b80) {
     T2[i37]
       = T1[0]
       + f8;
